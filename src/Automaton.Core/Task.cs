2bf385877fcf7b9088d2d749bb285e469f82d710
Logan Bussell false <loganbussell@microsoft.com>
2024-01-22 14:14:53 -0800

    Create initial source

---
+++ src/Automaton.Core/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace Automaton.Core;
+
+public record Task
+{
+    public Task(string name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+    }
+
+    public string Name { get; init; }
+
+    public Guid Id { get; init; }
+};
